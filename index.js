519b471df663818400599f1070e7a99a5847861a
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-08 15:56:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var uppercase = "HELLO!"
+var lowercase = "hello!"
+var love = "I love you, Grandma."
+
+function shout(lowercase){
+  return lowercase.toUpperCase()
+}
+function whisper(uppercase){
+  return uppercase.toLowerCase()
+}
+function logShout(lowercase){
+  console.log(lowercase.toUpperCase())
+}
+function logWhisper(uppercase){
+  console.log(uppercase.toLowerCase())
+}
+function sayHiToGrandma(love){
+    return `I love you, too.`
+}
+function sayHiToGrandma.toLowerCase(){
+  return `I can't hear you!`
+}
